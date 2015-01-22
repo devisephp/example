179bb020078eeb748a1f5a83c2562f204c0d82f9
Gary Williams <gary@lbm.co>
2015-01-22 10:31:15 -0500

    Added Navigation

--- html/js/brewers-and-beers.js
+++ html/js/brewers-and-beers.js
@@ -1,6 +1,24 @@
 $(function() {
 
 
+	function addNavigationListener() {
+		$(window).scroll(function (event) {
+			var _scroll = $(window).scrollTop();
+
+			if(_scroll > 440) {
+				$('#navigation').css({
+					top: 0,
+					opacity: 1
+				});
+			} else {
+				$('#navigation').css({
+					top: -150,
+					opacity: 0
+				});
+			}
+		});
+	}
+
 	function addLocalScroll() {
 		$.localScroll({
 			duration: 1500
@@ -10,6 +28,7 @@ $(function() {
 
 	function init() {
 		addLocalScroll();
+		addNavigationListener();
 	}
 
 	init();
