89162454dad0ff2f0e1655e7191b47e856811452
Chad Lockett <chad@lbm.co>
2015-05-13 13:25:35 -0400

    Updated include for Devise Assets. Few other adjustments to application layout.

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 	<title>Brewers &amp; Beers | Devise Demo Website</title>
-	<meta name="description" content="This is a demo website for Devise development examples and a sandbox to experiment in">
+	<meta name="description" content="Demo website for Devise Development Examples and a Sandbox to Experiment In!">
 
-	<link rel="stylesheet" type="text/css" href="/css/brewers-and-beers.css">
-	@include('devise::styles')
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/brewers-and-beers.css') }}">
+
+	@include('devise::assets')
 </head>
 
 <body>
@@ -15,13 +15,9 @@
 
 	@yield('content')
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/1.4.14/jquery.scrollTo.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-localScroll/1.3.5/jquery.localScroll.min.js"></script>
+    <script src="{{ URL::asset('js/brewers-and-beers.js') }}"></script>
 </body>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/1.4.14/jquery.scrollTo.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-localScroll/1.3.5/jquery.localScroll.min.js"></script>
-<script src="/js/brewers-and-beers.js"></script>
-
-@include('devise::scripts')
-
 </html>
